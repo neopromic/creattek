c58ddd29b1cb0a03777d25fa70ebf6a62e2d5862
NeO - Wesley Souza <65511208+neopromic@users.noreply.github.com>
2024-03-30 17:47:04 -0300

    feat: finished dialogContect initialLogic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,34 @@
-const btnContact = document.querySelector("#btnContact")
-const dialogContact = document.querySelector("#dialogContact")
+const btnContact = document.querySelector("#btnContact");
+const dialogContact = document.querySelector("#dialogContact");
+const dialogContactWPPBtn = document.querySelector("#dialogContactWPPBtn");
+const btnCloseContactDialog = document.querySelector("#btnCloseContactDialog");
+
 
 btnContact.addEventListener("click", () => {
-  Redirect("https://w.app/creattek");
-})
+  showDialog(dialogContact);
+  // Redirect("https://w.app/creattek");
+});
+
+dialogContactWPPBtn.addEventListener("click", () => {
+  return Redirect("https://w.app/creatttek");
+});
+
+btnCloseContactDialog.addEventListener("click", () => {
+  return closeDialog(dialogContact);
+});
 
 /*
-* Redirect function used globally in the application.
-* @params url String
-*/
+ * Redirect function used globally in the application.
+ * @params url String
+ */
 function Redirect(url) {
-  return window.location = url;
+  return (window.location = url);
 }
 
 function showDialog(dialogID) {
   return dialogID.showModal();
+}
+
+function closeDialog(dialogID) {
+  return dialogID.closeModal();
 }
